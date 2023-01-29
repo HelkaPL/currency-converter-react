81dd7d9c30841b903e5ed5b23b3c2c792a91d144
Jakub Helizanowicz <jakubhelizanowicz@gmail.com>
2023-01-29 22:08:29 +0100

    implement working script for exchange app

--- src/Exchanger/index.js
+++ src/Exchanger/index.js
@@ -1,36 +1,76 @@
+import { useState } from "react";
 import "./style.css"
 
 const Exchanger = () => {
+    const ratioEUR = 4.71;
+    const ratioPLN = 1;
 
+    const [inCurrency, setInputCurrency] = useState("1");
+
+    const onSelectInputChange = ({ target }) => setInputCurrency(target.value);
+    
+    const [inAmount, setInAmount] = useState("0.00");
+    
+    const onInAmountChange = ({ target }) => setInAmount(target.value);
+
+    const [outCurrency, setOutputCurrency] = useState("1");
+    
+    const onSelectOutputChange = ({ target }) => setOutputCurrency(target.value);
+    
+    const [outValue, setOutValue] = useState("");
+
+    const calculateValue = (inRatio, inAmount, outRatio) => {
+        const newOutValue = inAmount * (inRatio / outRatio);
+        setOutValue(newOutValue);
+
+    };
+
+    const onFormSubmit = (e) => {
+       e.preventDefault();
+       calculateValue(inCurrency, inAmount, outCurrency);
+
+    }
     return (
-        <form className="exchanger">
+        <form
+            onSubmit={onFormSubmit}
+            className="exchanger"
+        >
             <fieldset>
                 <legend className="exchanger__legend">Oszacuj wartość swojej waluty</legend>
                 <p>
                     <label className="exchanger__label">
-                        Posiadam walutę: <select className="js-inputCurrency">
-                            <option value="EUR" selected>Euro (EUR)</option>
-                            <option value="PLN" >Polski Złoty (PLN)</option>
+                        Posiadam walutę: 
+                        <select value={inCurrency} onChange={onSelectInputChange}>
+                            <option value={ratioEUR}>Euro (EUR)</option>
+                            <option value={ratioPLN}>Polski Złoty (PLN)</option>
                         </select>
                     </label>
                 </p>
                 <p>
                     <label className="exchanger__label">
-                        w ilości: <input className="js-inputValue" type="number" step="0.01" value="0" />
+                        w ilości: 
+                        <input 
+                          type="number"
+                          value={inAmount}
+                          step="0.01"
+                          min="0"
+                          onChange={onInAmountChange}
+                        />
                     </label>
                 </p>
                 <p>
                     <label className="exchanger__label">
-                        Chcę otrzymać: <select className="js-outputCurrency">
-                            <option value="EUR" >Euro (EUR)</option>
-                            <option value="PLN" selected>Polski Złoty (PLN)</option>
+                        Chcę otrzymać: 
+                        <select value={outCurrency} onChange={onSelectOutputChange}>
+                            <option value={ratioEUR}>Euro (EUR)</option>
+                            <option value={ratioPLN}>Polski Złoty (PLN)</option>
                         </select>
                     </label>
                 </p>
                 <p>Po sprawdzeniu kursów NBP <span className="js-exchangeRatio"></span></p>
                 <p>
                     <label className="exchanger__label">
-                        wychodzi że otrzymasz: <span className="exchanger__label--bold js-outputValue">0.00</span> szt.
+                        wychodzi że otrzymasz: <span className="exchanger__label--bold">{outValue.toFixed(2)}</span> szt.
                     </label>
                 </p>
             </fieldset>
