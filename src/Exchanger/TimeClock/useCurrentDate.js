003103ff2233ef5b77b9360eacb6476d6b093a1b
Jakub Helizanowicz <jakubhelizanowicz@gmail.com>
2023-02-01 12:46:15 +0100

    deleted commented code (not used)

--- src/Exchanger/TimeClock/useCurrentDate.js
+++ src/Exchanger/TimeClock/useCurrentDate.js
@@ -13,17 +13,5 @@ export const useCurrentDate = () => {
         };
     }, []);
 
-    // const formatDate = (date) => {
-    //     date.toLocaleString(undefined, {
-    //     weekday: "long",
-    //     day: "numeric",
-    //     month: "long",
-    //     year: "numeric",
-    //     hour: "2-digit",
-    //     minute: "2-digit",
-    //     second: "2-digit",
-    //     });
-    // };
-
     return currentDate;
 };
